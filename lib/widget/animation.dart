77382135f0be36d1df11bb1e4e9555ccfded984d
haim1121 <haim112177@naver.com>
2022-12-19 11:10:53 +0900

    12.19 haim (2) pull ok

--- lib/widget/animation.dart
+++ lib/widget/animation.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:simple_animations/simple_animations.dart';
+import 'package:flutter/animation.dart';
 
 enum AniProps { opacity, translateY }
 
