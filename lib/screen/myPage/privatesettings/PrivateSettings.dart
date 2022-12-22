422e9e8e6befdf05ae067a97d5f1b878eb186dfa
Bumee <favvuri@naver.com>
2022-12-22 14:45:35 +0900

    12-22 kibum (1) backend-modify & settings

--- lib/screen/myPage/privatesettings/PrivateSettings.dart
+++ lib/screen/myPage/privatesettings/PrivateSettings.dart
@@ -7,8 +7,11 @@ import 'package:daycus/screen/myPage/privatesettings/PasswordSetting.dart';
 import 'package:daycus/backend/UserDatabase.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import '../../../backend/Api.dart';
+import '../../../backend/NowTime.dart';
 
-
+import 'dart:convert';
 
 
 class PrivateSettings extends StatelessWidget {
@@ -28,7 +31,6 @@ class PrivateSettings extends StatelessWidget {
 
       await storage.delete(key: 'login');
     }
-
     checkUserState() async {
       userInfo = await storage.read(key: 'login');
       if (userInfo == null) {
@@ -39,6 +41,29 @@ class PrivateSettings extends StatelessWidget {
                 (route) => false);// 로그인 페이지로 이동
       } else {
         print('로그인 중');
+      }
+    }
+
+    remove_user() async {
+      try{
+        String cur = await NowTime('yyyy-MM-dd - HH:mm:ss');
+        var update_res = await http.post(Uri.parse(API.update), body: {
+          'update_sql': "UPDATE DayCus.user_table SET user_state = 'withdrawing', state_changed_time = '${cur}' WHERE (user_email = '${user_data['user_email']}')",
+        });
+
+        if (update_res.statusCode == 200) {
+          print("출력 : ${update_res.body}");
+          var resLogin = jsonDecode(update_res.body);
+          if (resLogin['success'] == true) {
+            logout();
+            checkUserState();
+          } else {
+            // 이름을 바꿀 수 없는 상황?
+          }
+        }
+      } catch (e) {
+        print(e.toString());
+        Fluttertoast.showToast(msg: e.toString());
       }
     }
 
@@ -167,7 +192,7 @@ class PrivateSettings extends StatelessWidget {
                     ),
 
                     onPressed: () {
-
+                      remove_user();
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
--- lib/screen/myPage/settings/NoticeSetting.dart
+++ lib/screen/myPage/settings/NoticeSetting.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:daycus/core/app_color.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'package:daycus/backend/UserDatabase.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import '../../../backend/Api.dart';
+import 'dart:convert';
 class NoticeSetting extends StatefulWidget {
   const NoticeSetting({Key? key}) : super(key: key);
 
@@ -14,6 +19,29 @@ class _NoticeSettingState extends State<NoticeSetting> {
 
   @override
   Widget build(BuildContext context) {
+
+    update_terms_market(value) async {
+      try{
+        var check = value == true ? 1 : 0;
+        var update_res = await http.post(Uri.parse(API.update), body: {
+          'update_sql': "UPDATE DayCus.user_table SET terms_market = '${check.toString()}' WHERE (user_email = '${user_data['user_email']}')",
+        });
+
+        if (update_res.statusCode == 200) {
+          print("출력 : ${update_res.body}");
+          var resLogin = jsonDecode(update_res.body);
+          if (resLogin['success'] == true) {
+            user_data['terms_market'] = value.toString();
+          } else {
+            // 이름을 바꿀 수 없는 상황?
+          }
+        }
+      } catch (e) {
+        print(e.toString());
+        Fluttertoast.showToast(msg: e.toString());
+      }
+    }
+
     return Scaffold(
       backgroundColor: AppColor.background,
       appBar: AppBar(
@@ -46,6 +74,7 @@ class _NoticeSettingState extends State<NoticeSetting> {
                     onChanged: (value){
                       setState(() {
                         isChecked[0] = value;
+                        isModified = value;
                       });
                     },
                   ),
@@ -71,22 +100,23 @@ class _NoticeSettingState extends State<NoticeSetting> {
                     height: 1.h,color: Colors.grey[400], margin: EdgeInsets.all(5),
                   ),
 
-                  SettingEachButtonCheck(
-                    title: "광고 ",
-                    subtitle: "(금주의 미션 안내 등)",
-                    value: isChecked[2],
-                    onTap: (){
-                    },
-                    onChanged: (value){
-                      setState(() {
-                        isChecked[2] = value;
-                      });
-                    },
-                  ),
 
-                  Container(
-                    height: 1.h,color: Colors.grey[400], margin: EdgeInsets.all(5),
-                  ),
+                  // SettingEachButtonCheck(
+                  //   title: "광고 ",
+                  //   subtitle: "(금주의 미션 안내 등)",
+                  //   value: isChecked[2],
+                  //   onTap: (){
+                  //   },
+                  //   onChanged: (value){
+                  //     setState(() {
+                  //       isChecked[2] = value;
+                  //     });
+                  //   },
+                  // ),
+                  //
+                  // Container(
+                  //   height: 1.h,color: Colors.grey[400], margin: EdgeInsets.all(5),
+                  // ),
 
                 ],
               ),
@@ -100,17 +130,41 @@ class _NoticeSettingState extends State<NoticeSetting> {
           ],
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        color: AppColor.happyblue,
+        child: Row(
+          children: [
+            SizedBox(
+              height: 70.h,
+              width: 412.w,
+              child:TextButton(
+                // 이거 생일 있을때랑 이름 있을때랑 각각 구분해서 잘 넣기.
+                // 팝업 띄우기, 규칙 만들기 (중복허용?)
+                  onPressed: () async {
+                    update_terms_market(isModified);
+                  },
+                  child: Text('수정하기',style: TextStyle(color: Colors.white, fontSize: 20.sp, fontFamily: 'korean', ) ) ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
 
 
+var isModified;
 
+var thing = int.parse(user_data['terms_market']);
+casting(){
+  if (thing==1){
+    return true;
+  }
+  else{return false;}
+}
 
-
-
-List<bool> isChecked = [true, true, true];
+List<bool> isChecked = [casting(), true];
 
 // 각 버튼
 class SettingEachButtonCheck extends StatelessWidget {
