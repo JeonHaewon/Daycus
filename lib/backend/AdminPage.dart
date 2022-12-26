1ba8e070f29285732e465b47a51a0e36163d0e45
haim1121 <haim112177@naver.com>
2022-12-26 16:43:45 +0900

    12.26 haim (3) admin page

---
+++ lib/backend/AdminPage.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:daycus/backend/Api.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:convert';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:daycus/backend/NowTime.dart';
+
+class AdminPage extends StatelessWidget {
+  const AdminPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    move_to_done_mission() async {
+      String now = await NowTime('yy/MM/dd - HH:mm:ss');
+      try {
+        var update_res = await http.post(Uri.parse(API.update), body: {
+          'update_sql': "INSERT INTO Done_mission  select * from (select * from do_mission where (select(datediff('${now}',mission_start) <= 14))) dating",
+        });
+
+        if (update_res.statusCode == 200 ) {
+          var resMission = jsonDecode(update_res.body);
+          // print(resMission);
+          if (resMission['success'] == true) {
+            Fluttertoast.showToast(msg: "정산이 완료되었습니다 !");
+
+          } else {
+            print("에러발생");
+            print(resMission);
+            Fluttertoast.showToast(msg: "다시 시도해주세요");
+          }
+
+        }
+      } on Exception catch (e) {
+        print("에러발생");
+        Fluttertoast.showToast(msg: "정산을 신청하는 도중 문제가 발생했습니다.");
+      }
+    }
+
+    delete_from_do_mission() async{
+
+      String now = await NowTime('yy/MM/dd - HH:mm:ss');
+      try {
+        var update_res = await http.post(Uri.parse(API.update), body: {
+          'update_sql': "DELETE FROM do_mission where (datediff('${now}',mission_start) <= 14);",
+        });
+
+        if (update_res.statusCode == 200 ) {
+          var resMission = jsonDecode(update_res.body);
+          // print(resMission);
+          if (resMission['success'] == true) {
+            Fluttertoast.showToast(msg: "삭제가 완료되었습니다 !");
+
+          } else {
+            print("에러발생");
+            print(resMission);
+            Fluttertoast.showToast(msg: "다시 시도해주세요");
+          }
+
+        }
+      } on Exception catch (e) {
+        print("에러발생");
+        Fluttertoast.showToast(msg: "삭제를 진행하는 도중 문제가 발생했습니다.");
+      }
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("개발자 페이지"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.w),
+        child: Column(
+          children: [
+            
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                primary: Colors.white,
+                onPrimary: Colors.black,
+                minimumSize: Size(365.w, 50.h),
+                textStyle: TextStyle(fontSize: 18.sp),
+              ),
+
+              onPressed: () {
+                move_to_done_mission();
+                bool finished = true;
+                if (finished) {
+                  delete_from_do_mission();
+                }
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("던미션 테스트용",style: TextStyle(fontFamily: 'korean', fontWeight: FontWeight.bold) ),
+                  Image.asset('assets/image/arrow-right1.png' )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
--- lib/screen/myPage/settings/Settings.dart
+++ lib/screen/myPage/settings/Settings.dart
@@ -1,15 +1,12 @@
+import 'package:daycus/backend/AdminPage.dart';
 import 'package:flutter/material.dart';
 import 'package:daycus/core/app_color.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:daycus/screen/myPage/settings/NoticeSetting.dart';
 import 'package:daycus/screen/myPage/settings/PictureSetting.dart';
 import 'package:daycus/screen/myPage/settings/PublicSetting.dart';
-import 'package:daycus/backend/NowTime.dart';
 import 'package:daycus/backend/UserDatabase.dart';
-import 'package:http/http.dart' as http;
-import 'package:daycus/backend/Api.dart';
-import 'dart:convert';
-import 'package:fluttertoast/fluttertoast.dart';
+
 
 
 class Settings extends StatelessWidget {
@@ -17,59 +14,6 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-    move_to_done_mission() async {
-      String now = await NowTime('yy/MM/dd - HH:mm:ss');
-      try {
-        var update_res = await http.post(Uri.parse(API.update), body: {
-          'update_sql': "INSERT INTO Done_mission  select * from (select * from do_mission where (select(datediff('${now}',mission_start) <= 14))) dating",
-        });
-
-        if (update_res.statusCode == 200 ) {
-          var resMission = jsonDecode(update_res.body);
-          // print(resMission);
-          if (resMission['success'] == true) {
-            Fluttertoast.showToast(msg: "정산이 완료되었습니다 !");
-
-          } else {
-            print("에러발생");
-            print(resMission);
-            Fluttertoast.showToast(msg: "다시 시도해주세요");
-          }
-
-        }
-      } on Exception catch (e) {
-        print("에러발생");
-        Fluttertoast.showToast(msg: "정산을 신청하는 도중 문제가 발생했습니다.");
-      }
-    }
-
-    delete_from_do_mission() async{
-
-      String now = await NowTime('yy/MM/dd - HH:mm:ss');
-      try {
-        var update_res = await http.post(Uri.parse(API.update), body: {
-          'update_sql': "DELETE FROM do_mission where (datediff('${now}',mission_start) <= 14);",
-        });
-
-        if (update_res.statusCode == 200 ) {
-          var resMission = jsonDecode(update_res.body);
-          // print(resMission);
-          if (resMission['success'] == true) {
-            Fluttertoast.showToast(msg: "삭제가 완료되었습니다 !");
-
-          } else {
-            print("에러발생");
-            print(resMission);
-            Fluttertoast.showToast(msg: "다시 시도해주세요");
-          }
-
-        }
-      } on Exception catch (e) {
-        print("에러발생");
-        Fluttertoast.showToast(msg: "삭제를 진행하는 도중 문제가 발생했습니다.");
-      }
-    }
 
     return Scaffold(
       backgroundColor: AppColor.background,
@@ -197,7 +141,12 @@ class Settings extends StatelessWidget {
                     ),
                   ),
 
+                  
+                  // 개발자 페이지
+                  if (user_data['user_state']=='admin')
                   SizedBox(height: 20.h,),
+
+                  if (user_data['user_state']=='admin')
                   ElevatedButton(
                     style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
@@ -210,16 +159,13 @@ class Settings extends StatelessWidget {
                     ),
 
                     onPressed: () {
-                      move_to_done_mission();
-                      bool finished = true;
-                      if (finished) {
-                        delete_from_do_mission();
-                      }
+                      Navigator.push(context, MaterialPageRoute(builder: (_)=> AdminPage()));
+
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text("던 미션 테스트용",style: TextStyle(fontFamily: 'korean', fontWeight: FontWeight.bold) ),
+                        Text("개발자 페이지",style: TextStyle(fontFamily: 'korean', fontWeight: FontWeight.bold) ),
                         Image.asset('assets/image/arrow-right1.png' )
                       ],
                     ),
