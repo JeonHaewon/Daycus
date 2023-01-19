ddf5d41aeb4d60cb5a0144be4e4874ac88960892
user <c12h34w@gmail.com>
2023-01-19 13:39:33 +0900

    1_19 haewon (3) change front

--- lib/screen/Friend/FriendPage.dart
+++ lib/screen/Friend/FriendPage.dart
@@ -99,90 +99,188 @@ class AddFriend extends StatelessWidget {
       backgroundColor: AppColor.background,
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(0, 20.h, 0, 0),
+          padding: EdgeInsets.fromLTRB(12.w, 25.h, 12.w, 0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+
             children: [
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(width: 10.w,),
-
-                  SizedBox(
-                    height: 80.h,
-                    width: 260.w,
-                    child : TextFormField(
-                      //controller: ,
-                      decoration: InputDecoration(
-                          hintText: "추가하고 싶은 친구의 닉네임을 입력하세요",
-                          hintStyle: TextStyle(fontFamily: 'korean',fontSize: 11.sp )
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(width: 8.w,),
-
-                  IconButton(
-                      icon: Icon(Icons.search),color: Colors.black,
-                    onPressed: () { }
-                  ),
-
-                ],
-              ),
-
-
-
               Container(
-                width: 300.w,
-                height: 50.h,
+                height: 310.h,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(5),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(25.w, 20.h, 25.w, 15.h),
+                  child: Column(
+                    children: [
+                      Text("친구 검색",style: TextStyle(fontSize: 14.sp, fontFamily: 'korean',fontWeight: FontWeight.bold) ),
 
-
-                    SizedBox(
-                      width: 160.w,
-                      height: 25.h,
-                      child: FittedBox(
-                        alignment: Alignment.center,
-                        fit: BoxFit.contain,
-
-                        child: Text("kdjkfjkjk",
-                            style: TextStyle(fontSize: 16.sp, fontFamily: 'korean', fontWeight: FontWeight.bold, ) ),
-                      ),
-                    ),
-
-
-                    SizedBox(width: 12.w,),
-
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)
-                        ),
-                        primary: Colors.indigo[600],
-                        onPrimary: Colors.white,
-                        minimumSize: Size(20.w, 28.h),
-                        textStyle: TextStyle(fontSize: 18.sp),
-                      ),
-
-                      onPressed: () {},
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Text("친구 요청",style: TextStyle(fontFamily: 'korean', fontSize: 10.sp) ),
+                          SizedBox(width: 4.w,),
+
+                          SizedBox(
+                            height: 80.h,
+                            width: 230.w,
+                            child : TextFormField(
+                              //controller: ,
+                              decoration: InputDecoration(
+                                  hintText: "추가하고 싶은 친구의 닉네임을 입력하세요",
+                                  hintStyle: TextStyle(fontFamily: 'korean',fontSize: 11.sp )
+                              ),
+                            ),
+                          ),
+
+                          SizedBox(width: 4.w,),
+
+                          IconButton(
+                              icon: Icon(Icons.search),color: Colors.black,
+                              onPressed: () { }
+                          ),
+
+                        ],
+                      ),
+
+
+
+                      Container(
+                        width: 300.w,
+                        height: 50.h,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+
+
+                            SizedBox(
+                              width: 160.w,
+                              height: 25.h,
+                              child: FittedBox(
+                                alignment: Alignment.center,
+                                fit: BoxFit.contain,
+                                child: Text("kdjkfjkjk",
+                                    style: TextStyle(fontSize: 16.sp, fontFamily: 'korean', fontWeight: FontWeight.bold, ) ),
+                              ),
+                            ),
+
+
+                            SizedBox(width: 12.w,),
+
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)
+                                ),
+                                primary: Colors.indigo[600],
+                                onPrimary: Colors.white,
+                                minimumSize: Size(20.w, 28.h),
+                                textStyle: TextStyle(fontSize: 18.sp),
+                              ),
+
+                              onPressed: () {},
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text("친구 요청",style: TextStyle(fontFamily: 'korean', fontSize: 10.sp) ),
+                                ],
+                              ),
+                            ),
+
+
+                          ],
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 20.h,),
+
+              Container(
+                height: 310.h,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+
+
+
+
+              Container(
+                width : 300.w,
+                height: 170.h,
+                decoration: BoxDecoration(
+                  color: Colors.blueGrey
+                ),
+                child: Scrollbar(
+                  isAlwaysShown: true,
+                  thickness: 8,
+                  radius: Radius.circular(10),
+                  //scrollbarOrientation: ScrollbarOrientation.right,
+                  child: NotificationListener<OverscrollIndicatorNotification>(
+                    onNotification: (OverscrollIndicatorNotification overScroll) {
+                      overScroll.disallowGlow();
+                      return false;
+                    },
+                    child: SingleChildScrollView(
+
+                      child: Column(
+                        children: [
+
+                          Container(
+                            width: 280.w,
+                            height: 45.h,
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey[50],
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+
+                          Container(
+                            width: 280.w,
+                            height: 45.h,
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey[50],
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+
+                          Container(
+                            width: 280.w,
+                            height: 45.h,
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey[50],
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+
+                          Container(
+                            width: 280.w,
+                            height: 45.h,
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey[50],
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+
+
+
+
+
                         ],
                       ),
                     ),
-
-
-                  ],
+                  ),
                 ),
               ),
 
